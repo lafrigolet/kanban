ed8e0de79a08056e0ad0c5dcf0a35095bbfaeecb
Luis A Frigolet <lafrigolet@gmail.com>
2025-11-01 14:12:09 +0100

    Added logos

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,37 @@ export default defineConfig({
   
   plugins: [
     tailwindcss(),
-    react()
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // actualiza automáticamente el SW
+      includeAssets: ['favicon-32x32.png', 'apple-touch-icon.png', '*.png'],
+      manifest: {
+        name: 'Mi App Kanban',
+        short_name: 'Kanban',
+        description: 'Un tablero Kanban hecho con React y Vite',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/kanbanchu-transparent-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/kanbanchu-transparent-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/kanbanchu-transparent-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
 })
