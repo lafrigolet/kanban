3af7a5f65147ba3d583ba5e0c1da2bee6da28a4e
Luis A Frigolet <lafrigolet@gmail.com>
2025-11-02 10:13:02 +0100

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kanbanchu/sw.js', { scope: '/kanbanchu/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
